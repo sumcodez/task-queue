2830c6972f15ffc9968c4e037b7c861fb4e8043e
sumcodez <cloud7suman@gmail.com>
2026-05-11 12:08:53 +0530

    redis config change

--- src/queue.js
+++ src/queue.js
@@ -6,6 +6,7 @@ import { Queue } from "bullmq";
 export const redisConnection = new Redis({
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
+    password: process.env.REDIS_PASSWORD,
     maxRetriesPerRequest: null,
 })
 
